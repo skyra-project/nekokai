e6e43c7ab9e3a2721cefcc2b1f385ef4e2448558
Jeroen Claassens <support@favware.tech>
2022-07-31 01:58:47 +0200

    chore: inside we both know what's been going on
    permalink: http://whatthecommit.com/6bda5f6eeb7347a96e07b6dea1d21afa

--- src/commands/other/waifu.ts
+++ src/commands/other/waifu.ts
@@ -2,23 +2,35 @@ import { BrandingColors } from '#lib/common/constants';
 import { LanguageKeys } from '#lib/i18n/LanguageKeys';
 import { EmbedBuilder } from '@discordjs/builders';
 import { parseURL } from '@sapphire/utilities';
-import { Command, RegisterCommand } from '@skyra/http-framework';
-import { applyLocalizedBuilder, resolveKey } from '@skyra/http-framework-i18n';
+import { Command, InteractionArguments, RegisterCommand } from '@skyra/http-framework';
+import { applyLocalizedBuilder, createSelectMenuChoiceName, resolveKey } from '@skyra/http-framework-i18n';
 
-@RegisterCommand((builder) => applyLocalizedBuilder(builder, LanguageKeys.Commands.Waifu.RootName, LanguageKeys.Commands.Waifu.RootDescription))
+@RegisterCommand(
+	(
+		builder //
+	) =>
+		applyLocalizedBuilder(builder, LanguageKeys.Commands.Waifu.RootName, LanguageKeys.Commands.Waifu.RootDescription).addStringOption((builder) =>
+			applyLocalizedBuilder(builder, LanguageKeys.Commands.Waifu.OptionRange) //
+				.addChoices(
+					createSelectMenuChoiceName(LanguageKeys.Commands.Waifu.KeyMediumQuality, { value: '50_000' }),
+					createSelectMenuChoiceName(LanguageKeys.Commands.Waifu.KeyHighQuality, { value: '75_000' }),
+					createSelectMenuChoiceName(LanguageKeys.Commands.Waifu.KeyLowQuality, { value: '100_000' })
+				)
+		)
+)
 export class UserCommand extends Command {
 	private readonly IMAGE_EXTENSION = /\.(bmp|jpe?g|png|gif|webp)$/i;
 
 	/**
 	 * Faces were generated with
-	 * - `𝜓 = 0.8` for images `#0–50,000` (medium quality/medium diversity).
-	 * - `𝜓 = 0.6` for images `#50,001–75,000` (high quality, low diversity).
-	 * - `𝜓 = 1.1` for images `#75,001–100,000` (low quality, high diversity) for a mix of good & interesting faces.
+	 * - `𝜓 = 0.8` for images `#0 – 50_000` (medium quality/medium diversity).
+	 * - `𝜓 = 0.6` for images `#50_001 – 75_000` (high quality, low diversity).
+	 * - `𝜓 = 1.1` for images `#75_001 – 100_000` (low quality, high diversity) for a mix of good & interesting faces.
 	 */
-	private readonly maximum = 100000;
+	private readonly maximum = '100_000';
 
-	public override chatInputRun(interaction: Command.Interaction) {
-		const url = `https://thiswaifudoesnotexist.net/example-${Math.floor(Math.random() * this.maximum)}.jpg`;
+	public override chatInputRun(interaction: Command.Interaction, { range }: InteractionArguments<Options>) {
+		const url = `https://thiswaifudoesnotexist.net/example-${Math.floor(Math.random() * parseInt(range ?? this.maximum, 10))}.jpg`;
 
 		const embed = new EmbedBuilder()
 			.setTitle('→')
@@ -40,4 +52,8 @@ export class UserCommand extends Command {
 		const parsed = parseURL(url);
 		return parsed && this.IMAGE_EXTENSION.test(parsed.pathname) ? parsed.href : undefined;
 	}
+}
+
+interface Options {
+	range?: string;
 }
