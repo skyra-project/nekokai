5b61d9d36a89b3b0acadce8b3df5e15b45bec943
Jeroen Claassens <support@favware.tech>
2022-07-31 02:23:08 +0200

    chore: this is how we generate our shit.
    permalink: http://whatthecommit.com/029836d13c0a94335947281b87fe3418

--- src/commands/other/waifu.ts
+++ src/commands/other/waifu.ts
@@ -5,39 +5,30 @@ import { parseURL } from '@sapphire/utilities';
 import { Command, InteractionArguments, RegisterCommand } from '@skyra/http-framework';
 import { applyLocalizedBuilder, createSelectMenuChoiceName, resolveKey } from '@skyra/http-framework-i18n';
 
-@RegisterCommand(
-	(
-		builder //
-	) =>
-		applyLocalizedBuilder(builder, LanguageKeys.Commands.Waifu.RootName, LanguageKeys.Commands.Waifu.RootDescription).addStringOption((builder) =>
-			applyLocalizedBuilder(builder, LanguageKeys.Commands.Waifu.OptionRange) //
-				.addChoices(
-					createSelectMenuChoiceName(LanguageKeys.Commands.Waifu.KeyMediumQuality, { value: 'medium' }),
-					createSelectMenuChoiceName(LanguageKeys.Commands.Waifu.KeyHighQuality, { value: 'high' }),
-					createSelectMenuChoiceName(LanguageKeys.Commands.Waifu.KeyLowQuality, { value: 'low' })
-				)
-		)
+@RegisterCommand((builder) =>
+	applyLocalizedBuilder(builder, LanguageKeys.Commands.Waifu.RootName, LanguageKeys.Commands.Waifu.RootDescription).addStringOption((builder) =>
+		applyLocalizedBuilder(builder, LanguageKeys.Commands.Waifu.OptionRange) //
+			.addChoices(
+				createSelectMenuChoiceName(LanguageKeys.Commands.Waifu.KeyAll, { value: 'all' }),
+				createSelectMenuChoiceName(LanguageKeys.Commands.Waifu.KeyMediumQuality, { value: 'medium' }),
+				createSelectMenuChoiceName(LanguageKeys.Commands.Waifu.KeyHighQuality, { value: 'high' }),
+				createSelectMenuChoiceName(LanguageKeys.Commands.Waifu.KeyLowQuality, { value: 'low' })
+			)
+	)
 )
 export class UserCommand extends Command {
-	private readonly SIZES = {
+	private readonly SIZES = Object.freeze({
 		medium: { min: 0, max: 50_000 },
 		high: { min: 50_000, max: 75_000 },
 		low: { min: 75_000, max: 100_000 },
 		all: { min: 0, max: 100_000 }
-	} as const;
-	private readonly IMAGE_EXTENSION = /\.(bmp|jpe?g|png|gif|webp)$/i;
+	});
 
-	/**
-	 * Faces were generated with
-	 * - `𝜓 = 0.8` for images `#0 – 50_000` (medium quality/medium diversity).
-	 * - `𝜓 = 0.6` for images `#50_001 – 75_000` (high quality, low diversity).
-	 * - `𝜓 = 1.1` for images `#75_001 – 100_000` (low quality, high diversity) for a mix of good & interesting faces.
-	 */
-	private readonly maximum = '100_000';
+	private readonly IMAGE_EXTENSION = /\.(bmp|jpe?g|png|gif|webp)$/i;
 
 	public override chatInputRun(interaction: Command.Interaction, options: InteractionArguments<Options>) {
 		const range = this.SIZES[options.range ?? 'all'];
-		const id = (Math.random() * (range.max - range.min)) + range.min;
+		const id = Math.random() * (range.max - range.min) + range.min;
 		const url = `https://thiswaifudoesnotexist.net/example-${id}.jpg`;
 
 		const embed = new EmbedBuilder()
@@ -56,12 +47,12 @@ export class UserCommand extends Command {
 	 * Parses an URL and checks if the extension is valid.
 	 * @param url The url to check
 	 */
-	private getImageUrl(url: string): string | undefined {
+	private getImageUrl(url: string): string {
 		const parsed = parseURL(url);
 		return parsed && this.IMAGE_EXTENSION.test(parsed.pathname) ? parsed.href : 'https://i.imgur.com/vKUeMoH.png';
 	}
 }
 
 interface Options {
-	range?: 'low' | 'medium' | 'high';
+	range?: 'low' | 'medium' | 'high' | 'all';
 }
