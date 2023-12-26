af9c80b4a73f1d941ca7b31393dc8c777fa5f5a0
Aura Román <kyradiscord@gmail.com>
2023-12-26 10:49:29 +0100

    refactor: update to HFx v2

--- src/lib/utilities/register-commands.ts
+++ src/lib/utilities/register-commands.ts
@@ -1,10 +1,10 @@
 import { envIsDefined, envParseString } from '@skyra/env-utilities';
-import { applicationCommandRegistry } from '@skyra/http-framework';
+import { container } from '@skyra/http-framework';
 
 export async function registerCommands() {
 	if (envIsDefined('REGISTRY_GUILD_ID')) {
-		await applicationCommandRegistry.pushAllCommandsInGuild(envParseString('REGISTRY_GUILD_ID'));
+		await container.applicationCommandRegistry.pushAllCommandsInGuild(envParseString('REGISTRY_GUILD_ID'));
 	} else {
-		await applicationCommandRegistry.pushGlobalCommands();
+		await container.applicationCommandRegistry.pushGlobalCommands();
 	}
 }
